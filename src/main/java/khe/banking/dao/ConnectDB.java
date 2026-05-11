c59a115bd55215d20b9b1ee8e12e662179409893
kwonhe1004 <132746391+kwonhe1004@users.noreply.github.com>
2026-05-11 14:47:21 -0500

    Update database connection details in ConnectDB

--- src/main/java/khe/banking/dao/ConnectDB.java
+++ src/main/java/khe/banking/dao/ConnectDB.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectDB {
-	public static final String URL = "jdbc:mysql://localhost:3306/testdb";
+	public static final String URL = "jdbc:mysql://localhost:3306/bank_mgmt";
 	public static final String USERNAME = "root";
 	public static final String PASSWORD = "";
 
